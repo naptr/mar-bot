a6e462eb5e9a9acb1b244defd02dcb813e8b2315
putra <krishna.putra332@gmail.com>
2022-02-17 13:52:08 +0700

    add silly code

--- data/events/ready.js
+++ data/events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    console.log(`Bot is Ready, Logged in as ${client.user.tag}`);
+    console.log(`Bot is Ready, Logged in as ${client.user.tag} from Discord`);
   },
 };
